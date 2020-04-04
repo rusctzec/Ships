d0e2b2cb3ddb37fb3686357329d8f1e060a95f5d
Daniel V <main@dvasquez4155.com>
2020-04-04 12:19:52 -0700

    Added old bit theme

--- server.js
+++ server.js
@@ -28,7 +28,10 @@ app.use(passport.session());
 // Static directory
 app.use(express.static("app/public"));
 
+app.use('/js', express.static(__dirname + '/node_modules/bootstrap/dist/js')); // redirect bootstrap JS
 app.use('/js', express.static(__dirname + '/node_modules/jquery/dist')); // redirect JS jQuery
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css')); // redirect CSS bootstrap
+app.use('/css', express.static(__dirname + '/node_modules/nes.css/css')); // redirect CSS bootstrap
 
 // Set Handlebars.
 var exphbs = require("express-handlebars");
