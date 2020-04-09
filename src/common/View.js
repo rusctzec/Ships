c188b1b2c6485d419666c953f681a44e3825fd1e
Daniel V <main@dvasquez4155.com>
2020-04-08 20:00:33 -0700

    Added health logic

---
+++ src/common/View.js
@@ -0,0 +1,23 @@
+// const htmlCanvas = document.getElementById("health"),
+// armor = document.getElementById("armor");
+const maxHealth = 20,
+heart = "nes-icon heart",
+half = "is-half",
+empty = "is-transparent"
+function updateHealth(health) {
+    var healthBar = "";
+    for (var i = 0; i < Math.floor((health)/2); i++) {
+        healthBar += `<i class='${heart}'></i>`;
+    }
+    if (health % 2) {
+        healthBar += `<i class='${heart} ${half}'></i>`;
+    }
+    for (var i = 0; i < ((maxHealth/2) - Math.round((health)/2)); i++) {
+        healthBar += `<i class='${heart} ${empty}'></i>`;
+    }
+    console.log(healthBar)
+}
+function update(armor) {
+    armor.value = armor;
+}
+updateHealth(9)
