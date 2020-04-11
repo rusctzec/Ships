aa68785b36bcc07d695b75984ec4306774e30db4
Daniel V <main@dvasquez4155.com>
2020-04-11 10:57:49 -0700

    Added register/login script

--- src/public/assets/js/game.js
+++ src/public/assets/js/game.js
@@ -49,3 +49,10 @@ function updateMovement() {
     movement.left = key.KeyA;
     movement.right = key.KeyD;
 }
+// $(document).ready(function() {
+//   // This file just does a GET request to figure out which user is logged in
+//   // and updates the HTML on the page
+//   $.get("/api/user_data").then(function(data) {
+//     $(".member-name").text(data.email);
+//   });
+// });
